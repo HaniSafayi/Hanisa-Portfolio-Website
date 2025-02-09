dbf403b4ec2ebe5b1fafbb460084e0bf6be43cee
hanigsafayi <hanisagammada288@gmail.com>
2025-02-09 11:34:19 -0800

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./app.scss";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Test from "./Test";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
@@ -8,41 +7,35 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
-// import Services from "./components/services/Services";
+import Services from "./components/services/Services";
+
 
 const App = () => {
   return (
-    <Router>
+    <div>
       <Cursor />
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <section id="Homepage">
-                <Hero />
-              </section>
-              <section id="Portfolio">
-                <Parallax type="portfolio" />
-              </section>
-              <section id="Skills">
-                <Skills />
-              </section>
-              {/* <section id="Services">
-                <Services />
-              </section> */}
-              <Portfolio />
-              <section id="Contact">
-                <Contact />
-              </section>
-            </>
-          }
-        />
-      </Routes>
-    </Router>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <section id="Skills">
+        <Skills />
+      </section>
+      {/*<section id="Services">
+        <Services />
+      </section>*/}
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
   );
 };
 
 export default App;
-
